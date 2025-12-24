ad6672e6db35ab13f01e820ea6dfb4ce2c3f3b91
gpaganini <giovanipaganini@outlook.com>
2025-12-24 11:59:26 -0300

    Add BRLF_Reb_PMC template with Brazilian flag texture

--- A3UE_BRLF/addons/templates/Templates/Templates.hpp
+++ A3UE_BRLF/addons/templates/Templates/Templates.hpp
@@ -12,4 +12,15 @@ class Templates
         file = "BRLF_Reb";
         description = "";
     };
+
+    class BRLF_Reb_PMC : Vanilla_Base
+    {
+        requiredAddons[] = {"CUP_Creatures_People_Civil_Russia", "CUP_BaseConfigs", "CUP_AirVehicles_Core", "BRAF_Air"};
+        basepath = QPATHTOFOLDER(Templates\BRLF);
+        flagTexture = QPATHTOFOLDER(Templates\BRLF\flag_brazil.paa);
+        side = "Reb";
+        name = "BR PMC";
+        file = "BRLF_Reb_PMC";
+        description = "";
+    };
 };
